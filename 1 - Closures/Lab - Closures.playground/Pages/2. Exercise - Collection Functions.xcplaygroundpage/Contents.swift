6fb119dd5352fbeb77108c8df4fe1ecb60301bf1
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-20 09:09:48 -0700

    Completed the Closures Lab

--- 1 - Closures/Lab - Closures.playground/Pages/2. Exercise - Collection Functions.xcplaygroundpage/Contents.swift
+++ 1 - Closures/Lab - Closures.playground/Pages/2. Exercise - Collection Functions.xcplaygroundpage/Contents.swift
@@ -4,8 +4,8 @@
  Define a function `forwards` that has two `String` arguments, `name1` and `name2`, and returns a `Bool`. Within the definition of the function, print each of the arguments, then return whether or not `name1` is less than `name2`. You can use comparison operators, such as `<` and `>`, to compare alphabetical order in strings.
  */
 func forwards(_ nameOne: String, _ nameTwo: String) -> Bool {
-	print("\(nameOne)")
-	print("\(nameTwo)")
+//	print("\(nameOne)")
+//	print("\(nameTwo)")
 	return nameOne < nameTwo
 }
 
@@ -21,11 +21,17 @@ let friendsAndFamily: [String] = ["Brett",
 
 let sortedCollection1: [String] = friendsAndFamily.sorted(by: forwards)
 //:  Using your initial collection of unsorted friends and family names, call the collection's `sorted(by:)` function, but pass in your own closure instead of the `forwards` function. The closure should sort the collection in the same way as `forwards`. Be sure to include parameter names, parameter types, and the `return` statement within your closure. Store the result in `sortedCollection2`, then print the result.
-
+let sortedCollection2: [String] = friendsAndFamily.sorted {
+	personOne, personTwo in
+	return personOne < personTwo
+}
 
 //:  Similar to the previous exercise, call the collection's `sorted(by:)` function, but remove as much of the unnecessary closure syntax as you can. Store the result in `sortedCollection3`, then print the result.
+let sortedCollection3: [String] = friendsAndFamily.sorted {	$0 < $1	}
 
-
+for person in sortedCollection3 {
+	print(person)
+}
 /*:
 [Previous](@previous)  |  page 2 of 3  |  [Next: Exercise - Collection Functions](@next)
  */
