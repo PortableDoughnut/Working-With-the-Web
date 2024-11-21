5f0c4116345fb45f05c0354ec48323c145c6e955
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-21 09:25:39 -0700

    Completed step one.

--- Contest/Contest/ViewController.swift
+++ Contest/Contest/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var emailTextField: UITextField!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func emailSubmitButtonPressed(_ sender: UIButton) {
+		
+	}
+	
 }
 
