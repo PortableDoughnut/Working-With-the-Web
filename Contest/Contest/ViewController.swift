8b6306876471ca3db2aeae4a7d6e314113ad33e1
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-21 10:03:54 -0700

    Completed the Contest lab.

--- Contest/Contest/ViewController.swift
+++ Contest/Contest/ViewController.swift
@@ -14,10 +14,28 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
+	
 	@IBAction func emailSubmitButtonPressed(_ sender: UIButton) {
-		
+		if emailTextField.text?.isEmpty == true ||
+			emailTextField.text?.contains(/[:word:]@[:alpha:].[:alpha:]/) == false {
+			emailTextField.placeholder = "Please enter your email address."
+			
+			UIView.animate(withDuration: 0.1, animations: {
+				//				self.emailTextField.transform = CGAffineTransform(scaleX: , y:  dropleぽ)
+				self.emailTextField.transform = CGAffineTransform(rotationAngle: -0.0125)
+			}) { _ in
+				UIView.animate(withDuration: 0.1, animations: {
+					self.emailTextField.transform = CGAffineTransform(rotationAngle: 0.0125)
+				}) { _ in
+					UIView.animate(withDuration: 0.1, animations: {
+						self.emailTextField.transform = CGAffineTransform.identity
+					})
+				}
+			}
+		}
+		else {
+			performSegue(withIdentifier: "EmailEnteredSegue", sender: nil)
+		}
 	}
 	
 }
-
