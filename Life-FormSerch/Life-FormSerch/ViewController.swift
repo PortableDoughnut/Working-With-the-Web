0c31524761681092dd0abf6a6d078a9e971c612d
PortableDoughnut <gwen.thelin@gmail.com>
2024-12-11 09:42:13 -0700

    Added Accent Colour and App Icons.

---
+++ Life-FormSerch/Life-FormSerch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life-FormSerch
+//
+//  Created by Gwen Thelin on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
